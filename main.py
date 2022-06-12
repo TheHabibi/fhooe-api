f0d464d878f39bb20ce749975c45c5cadd15d985
TheHabibi <celebialieren@gmail.com>
2022-06-12 03:32:51 +0200

    update

--- main.py
+++ main.py
@@ -3,29 +3,11 @@ from scraper import *
 
 app = FastAPI()
 
-@app.get('/hagenberg')
+@app.get('/bachelors')
 def getHagenberg():
     return{
-        "Hagenberg":hagenberg
-    }
-
-@app.get('/linz')
-def getLinz():
-    return{
-        "Linz":linz
-    }
-
-@app.get('/steyr')
-def getWels():
-    return{
-        "Steyr":steyr
-    }
-
-@app.get('/wels')
-def getWels():
-    return{
+        "Hagenberg":hagenberg,
+        "Linz":linz,
+        "Steyr":steyr,
         "Wels":wels
     }
-
-
-
